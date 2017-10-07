fc7a640eb3b6193a7ac0d9345de8a70f9f05cfa5
miriambej <justme0112@hotmail.com>
2017-10-07 11:41:05 +1100

    create contact page

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// req = request, res = respond
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'CodeTogether - a platform for sharing code.' });
 });
 
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'CodeTogether - a platform for sharing code.' });
+});
+
+// contact as well as getting, we need to post.
+router.route('/contact')
+  .get(function(req, res, next) {
+    res.render('contact', { title: 'CodeTogether - a platform for sharing code.'});
+  })
+  // when you click submit, it runs the post request and renders thank page.
+  .post(function(req, res, next) {
+    res.render('thank', { title: 'CodeTogether - a platform for sharing code.'});
 });
 
 module.exports = router;
