2558b1753fe2c8b5562c79739cc50ab2de5aca4e
miriambej <justme0112@hotmail.com>
2017-10-11 02:49:11 +1100

    keys

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     service: 'Gmail',
     auth: {
       user: 'heroku.miriam23@gmail.com',
-      pass: '12345678'
+      pass: 'GM_1'
     }
   },
   dbConnstring: 'mongodb://admin:123@ds113775.mlab.com:13775/codetogether',
