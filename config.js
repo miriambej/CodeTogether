65d673d27826d96bb16ea2947075be34967a3a6e
miriambej <justme0112@hotmail.com>
2017-10-11 14:40:02 +1100

    keys

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 'use strict'
 
 module.exports = {
@@ -6,7 +5,7 @@ module.exports = {
     service: 'Gmail',
     auth: {
       user: 'heroku.miriam23@gmail.com',
-      pass: process.env.GM_1
+      pass: process.env['GM_1']
     }
   },
   dbConnstring: 'mongodb://admin:123@ds113775.mlab.com:13775/codetogether',
